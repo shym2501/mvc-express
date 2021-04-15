8a3deadc06dd8a673e5d41ebdd013d9ecfde63f7
shym2501 <sulthannurya@gmail.com>
2021-04-15 23:12:18 +0800

    update

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Import Routers
+const Home = require('./routes/home.js')
+
+//Express config
+app.use(express.json())
+app.use(express.urlencoded({extended: false }))
+app.use(express.json())
+
+//Use Router
+app.use(Home)
 
 app.listen(port, () => {
   console.log(`Running at http://localhost:${port}`)
